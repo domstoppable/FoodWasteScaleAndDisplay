ea3ebfce7809bbe73356f449040f458b85fca50b
Dominic Canare <dom@greenlightgo.org>
2020-03-13 21:32:05 -0500

    Update pins for new board + add LED pin

--- ScaleFirmware/ScaleFirmware.ino
+++ ScaleFirmware/ScaleFirmware.ino
@@ -1,9 +1,13 @@
 #include <HX711_ADC.h>
 #include "PIRSensor.h"
 
-HX711_ADC loadCell(3, 2); // DT, SCK
-PIRSensor motionSensor(4);
+HX711_ADC loadCell(2, 3); // DT, SCK
 long lastLoadCellUpdate = 0l;
+
+PIRSensor motionSensor(4);
+
+int ledPin = 11;
+long ledOnTime = 0l;
 
 void setup() {
 	Serial.begin(9600);
@@ -12,6 +16,7 @@ void setup() {
 	loadCell.start(2000);
 	loadCell.setCalFactor(696.0); // user set calibration factor (float)
 
+	pinMode(ledPin, OUTPUT);
 	motionSensor.begin();
 }
 
@@ -22,14 +27,21 @@ void loop(){
 		if(motionSensor.state){
 			Serial.println("m");
 		}
+    digitalWrite(ledPin, HIGH);
+    ledOnTime = currentTime;
 	}
 	loadCell.update();
 
-	if (currentTime > lastLoadCellUpdate + 1000) {
+	if (currentTime > lastLoadCellUpdate + 1000l) {
 		float i = loadCell.getData();
 		float v = loadCell.getCalFactor();
 		Serial.print("w");
 		Serial.println(i);
 		lastLoadCellUpdate = currentTime;
+	}
+        
+	if(ledOnTime > 0l && currentTime > ledOnTime + 1000l){
+		ledOnTime = 0l;
+		digitalWrite(ledPin, LOW);
 	}
 }
